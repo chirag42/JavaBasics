6172d40fe5e8d999a97c4fe1f09ae1b625c9c68a
chirag42 <chiragnagpal39@gmail.com>
2025-04-13 18:06:46 -0400

    First commit with basic implementation of multithreading and thread life cycle

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+class B implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " CN here");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+class A extends Thread {
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " World");
+
+
+    }
+}//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        A a = new A();
+        a.start();
+        B b = new B();
+        Thread t1 = new Thread(b);
+        System.out.println(t1.getState());
+        t1.start();
+        System.out.println(t1.getState());
+        Thread.sleep(100);
+        System.out.println(t1.getState());
+        t1.join();
+        System.out.println(t1.getState());
+        System.out.println(Thread.currentThread().getName() + " Hello");
+    }
+}
