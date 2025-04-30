7e7f56c4a77fe636c3de1446b29775a795048970
chirag42 <chiragnagpal39@gmail.com>
2025-04-29 20:05:23 -0400

    Using lambda for Runnable interface implementation as an example.

---
+++ src/LambdaExpression.java
@@ -0,0 +1,21 @@
+interface Add {
+    // using lambda for interface with single abstract method.
+    int add(int x, int y);
+}
+
+public class LambdaExpression {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Add addImpl = (x,y) -> x + y;
+
+        System.out.println(addImpl.add(1,2));
+
+        Thread t1 = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName());
+        });
+        t1.start();
+        Thread.sleep(100);
+        t1.setName("Thread-1");
+    }
+}
