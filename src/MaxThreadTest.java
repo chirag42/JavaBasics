02cfb8a5f98b3071b2595b511ac91abb0080bf67
chirag42 <chiragnagpal39@gmail.com>
2025-05-02 16:53:20 -0400

    Just to check what is max number of threads i can use at any point with my system.

--- src/MaxThreadTest.java
+++ src/MaxThreadTest.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class MaxThreadTest {
+    public static void main(String[] args) {
+        int count = 0;
+        try {
+            while (true) {
+                new Thread(() -> {
+                    try { Thread.sleep(1000000); } catch (Exception ignored) {}
+                }).start();
+                count++;
+            }
+        } catch (Error e) {
+            System.out.println("Max threads: " + count);
+        }
+    }
 }
