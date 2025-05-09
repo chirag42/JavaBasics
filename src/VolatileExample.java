d2d6284f264f6b0e22f8a767bc64a690d1e6577a
chirag42 <chiragnagpal39@gmail.com>
2025-05-08 20:46:46 -0400

    Understood use of volatile, Makes sure that the shared resource parameter(s) marked volatile is/are read from memory and not threads cache/stack.

--- src/VolatileExample.java
+++ src/VolatileExample.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+class SharedObject {
+    private volatile boolean flag = false;
+    public boolean getFlagIfTrue() {
+        while(!flag) {
+            // nothing
+        }
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        System.out.println("WriterThread has set flag to true ");
+        this.flag = flag;
+    }
+}
 
 public class VolatileExample {
+    public static void main(String[] args) {
+        SharedObject sharedObject = new SharedObject();
+        Thread writingThread = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            }catch (InterruptedException ignored) {
+
+            }
+            sharedObject.setFlag(true);
+        });
+        Thread readingThread = new Thread(() -> {
+            boolean flagval = sharedObject.getFlagIfTrue();
+            System.out.println("Flag set to " + flagval + " by writing thread");
+        });
+        writingThread.start();
+        readingThread.start();
+
+    }
 }
