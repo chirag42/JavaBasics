394981a9b2bc9f5d13c8c665c9c8e9adfb186323
chirag42 <chiragnagpal39@gmail.com>
2025-05-27 22:43:45 -0400

    Learning about futures and its methods

--- src/FuturesTest.java
+++ src/FuturesTest.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.concurrent.*;
 
 public class FuturesTest {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        Future<Integer> future = executorService.submit(() -> {
+            Thread.sleep(2000);
+            System.out.println("Submit executed task");
+           return 20;
+        });
+        Thread.sleep(1000);
+        future.cancel(false);
+
+        try {
+            System.out.println(future.isCancelled());
+            System.out.println(future.isDone());
+            System.out.println(future.get());
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (CancellationException e){
+            System.out.println("Future Cancellation - " + e.toString());
+            //throw new RuntimeException(e);
+        }finally {
+            executorService.shutdown();
+        }
+
+
+    }
 }
